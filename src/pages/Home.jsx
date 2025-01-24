7e53a6f3ee9fa02a1854f3072d08be6aaf0a5545
Teodora <tedy.aa@abv.bg>
2025-01-24 20:16:20 +0200

    added hero to main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 const Home = () => {
-    return(
+    return (
         <div>
-
+            <Hero/>
         </div>
     )
 }
