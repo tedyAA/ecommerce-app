82e47167b310ac12c347f21088ccbee8c9cdacf1
Teodora <tedy.aa@abv.bg>
2025-01-24 20:16:31 +0200

    refactored routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,19 @@ import Orders from "./pages/Orders.jsx";
 import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
-    return(
+    return (
         <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-          <NavBar/>
+            <NavBar/>
             <Routes>
-              <Route path="/" component={Home} />
-              <Route path="/collection" component={Collection} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/product/:productId" component={Product} />
-              <Route path="/card" component={Cart} />
-              <Route path="/login" component={Login} />
-              <Route path="/place-order" component={PlaceOrder} />
-              <Route path="/orders" component={Orders} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/collection" element={<Collection/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/product/:productId" element={<Product/>}/>
+                <Route path="/card" element={<Cart/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/place-order" element={<PlaceOrder/>}/>
+                <Route path="/orders" element={<Orders/>}/>
             </Routes>
         </div>
     )
