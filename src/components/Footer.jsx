457685a5bd7f4f4c6efecbe36914accc9f0658f7
Teodora <tedy.aa@abv.bg>
2025-01-26 22:00:38 +0200

    created page footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {assets} from "../assets/assets.js";
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+                <div>
+                    <img src={assets.logo} className='mb-5 w-32'/>
+                    <p className='w-full md:w-2/3 text-gray-600'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
+                        <li>+359-359-359-359</li>
+                        <li>contact@aboutme.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr/>
+            <p className='py-5 text-sm text-center'>Copyright 2025 About me - All rights reserved</p>
+        </div>
+    )
+}
+export default Footer;
