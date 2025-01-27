c7a0073e311ee16a40eb1b233e131bdd3f6d4ef2
Teodora <tedy.aa@abv.bg>
2025-01-27 14:46:57 +0200

    added filter and searchbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React, {useContext} from 'react';
+import {ShopContext} from "../context/ShopContext.jsx";
+
+const SearchBar = () => {
+    const {search, setSearch, showSearch, setShowSearch} = useContext(ShopContext)
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default SearchBar;
