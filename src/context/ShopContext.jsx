858e51138a1b63fb4a0879884379514b91361235
Teodora <tedy.aa@abv.bg>
2025-01-29 12:25:14 +0200

    created place order page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import {createContext, useState} from "react";
 import {products} from "../assets/assets";
 import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,7 +12,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
-
+    const navigate = useNavigate()
     const addToCart = async (itemId, size) => {
 
         if (!size) {
@@ -86,7 +87,8 @@ const ShopContextProvider = (props) => {
         cartItems, addToCart,
         getCartCount,
         updateQuantity,
-        getCartAmount
+        getCartAmount,
+        navigate
     }
     return (
         <ShopContext.Provider value={value}>
