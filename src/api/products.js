6330d800c82ac29ab3d518db2a81c7a1c60ecb2f
Teodora <tedy.aa@abv.bg>
2025-08-25 17:40:40 +0300

    added parameters

--- src/api/products.js
+++ src/api/products.js
@@ -2,9 +2,21 @@
 import axios from "axios";
 
 export default {
-    index() {
-        return axios.get("http://127.0.0.1:3000/api/products");
+    index({ bestseller, per } = {}) {
+        const params = [];
+
+        if (per !== undefined && per !== null) {
+            params.push(`per=${per}`);
+        }
+
+        if (bestseller !== undefined && bestseller !== null) {
+            params.push(`bestseller=${bestseller}`);
+        }
+        const query = params.length ? `?${params.join('&')}` : '';
+
+        return axios.get(`http://127.0.0.1:3000/api/products${query}`);
     },
+
     show(id) {
         return axios.get(`http://127.0.0.1:3000/api/products/${id}`);
     }
