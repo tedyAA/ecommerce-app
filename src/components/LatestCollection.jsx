17ebd82109294e16e8cb1946f5c16cda99baf253
Teodora <tedy.aa@abv.bg>
2025-08-25 17:41:05 +0300

    refactored to use api

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,34 +1,46 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {ShopContext} from "../context/ShopContext.jsx";
+import React, { useEffect, useState} from 'react';
 import Title from "./Title.jsx";
 import ProductItem from "./ProductItem.jsx";
+import productsApi from "../api/products.js";
 
 const LatestCollection = () => {
 
-    const {products} = useContext(ShopContext);
-    const [latestProducts, setLatestProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        setLatestProducts(products.slice(0, 10))
+        productsApi
+            .index({per: 5})
+            .then((response) => {
+                setProducts(response.data);
+                console.log(products);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError(err);
+                setLoading(false);
+            });
     }, []);
-    return (
+    return !loading ? (
         <div className='my-10'>
             <div className='text-center py-8 text-3xl'>
                 <Title text1={'LATEST'} text2={'COLLECTION'}/>
-                <p className='w-3/4 m--auto text-xs sm:text-sm md:text-base text-gray-600'>
+                <p className='w-3/4 m--auto text-xs sm:text-sm md:text-base text-center text-gray-600'>
                     Lorem Ipsum is a simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
                     the industry.
                 </p>
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
-                    latestProducts.map((product, i) => (
-                        <ProductItem key={i} id={product._id} image={product.image} name={product.name}
-                                     price={product.price}/>
+                    products.map((product, i) => (
+                        <ProductItem product={product} key={i} />
                     ))
                 }
             </div>
         </div>
+    ) : (
+        <p>Loading Please Wait ...</p>
     )
 }
 export default LatestCollection;
