c159cf51eec592dd8c2c0d57cab31416b0e521d8
Teodora <tedy.aa@abv.bg>
2025-08-25 17:40:56 +0300

    refactored to show only bestsellers

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,17 +1,27 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {ShopContext} from "../context/ShopContext.jsx";
+import React, { useEffect, useState} from 'react';
 import Title from "./Title.jsx";
 import ProductItem from "./ProductItem.jsx";
+import productsApi from "../api/products.js";
 
 const BestSeller = () => {
 
-    const {products} = useContext(ShopContext);
     const [bestSeller, setBestSeller] = useState([]);
+    const [loading, setLoading] = useState([]);
+    const [error, setError] = useState([]);
 
     useEffect(() => {
-        const bestProduct = products.filter((product) => product.bestseller);
-        setBestSeller(bestProduct.slice(0, 5))
+        productsApi
+            .index({bestseller: true})
+            .then((response) => {
+                setBestSeller(response.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError(err);
+                setLoading(false);
+            });
     }, []);
+
     return (
         <div className='my-10'>
             <div className='text-center text-3x py-8'>
@@ -22,8 +32,7 @@ const BestSeller = () => {
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {bestSeller.map((product, index) => (
-                    <ProductItem key={index} id={product._id} price={product.price} name={product.name}
-                                 image={product.image}/>
+                    <ProductItem key={index} product={product}/>
                 ))}
             </div>
         </div>
