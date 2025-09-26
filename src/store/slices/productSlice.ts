f87f56b3e18d533b2b0bcbcb9350f70d18771797
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2025-09-26 14:24:36 +0300

    refactored slice

--- src/store/slices/productSlice.ts
+++ src/store/slices/productSlice.ts
@@ -1,9 +1,30 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface Product {
+export interface Category {
     id: number;
     name: string;
+    description: string | null;
+}
+
+export interface Type {
+    id: number;
+    name: string;
+    description: string | null;
+}
+
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
     price: number;
+    category_id: number;
+    type_id: number;
+    created_at: string;
+    updated_at: string;
+    bestseller: boolean;
+    category: Category;
+    type: Type;
+    image_urls: string[];
 }
 
 interface ProductState {
