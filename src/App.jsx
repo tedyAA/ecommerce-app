b7ed5cdf4b24daa1aef5eeca727927006c9d8f2c
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2025-09-26 13:38:24 +0300

    added persisted state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Collection from "./pages/Collection.jsx";
 import About from "./pages/About.jsx";
@@ -13,32 +13,37 @@ import Footer from "./components/global/Footer.jsx";
 import SearchBar from "./components/global/SearchBar.jsx";
 import PlaceOrder from "./pages/PlaceOrder.jsx";
 import OrderSuccess from "./pages/OrderSuccess.jsx";
-import {ToastContainer} from 'react-toastify';
-import { store } from "./store";
-import {Provider} from "react-redux";
-const App = () => {
+import { ToastContainer } from "react-toastify";
 
+import { store, persistor } from "./store"; // import persistor
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+const App = () => {
     return (
-        <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-            <Provider store={store}>
-            <ToastContainer/>
-            <NavBar/>
-            <SearchBar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/collection" element={<Collection/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/account" element={<Account/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="/product/:productId" element={<Product/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/place-order" element={<PlaceOrder/>}/>
-                <Route path="/order-success" element={<OrderSuccess/>}/>
-            </Routes>
-            <Footer/>
-            </Provider>
-        </div>
-    )
-}
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+                    <ToastContainer />
+                    <NavBar />
+                    <SearchBar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/collection" element={<Collection />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/account" element={<Account />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/product/:productId" element={<Product />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/place-order" element={<PlaceOrder />} />
+                        <Route path="/order-success" element={<OrderSuccess />} />
+                    </Routes>
+                    <Footer />
+                </div>
+            </PersistGate>
+        </Provider>
+    );
+};
+
 export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,14 +4,52 @@ import cartReducer from "./slices/cartSlice";
 import orderReducer from "./slices/orderSlice";
 import productReducer from "./slices/productSlice";
 
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
+
+// Persist config for user slice
+const userPersistConfig = {
+    key: "user",
+    storage,
+    whitelist: [
+        "token",
+        "email",
+        "firstName",
+        "lastName",
+        "id",
+        "avatar_url",
+        "isAuthenticated",
+    ],
+};
+
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+
 export const store = configureStore({
     reducer: {
-        user: userReducer,
+        user: persistedUserReducer,
         cart: cartReducer,
         orders: orderReducer,
         products: productReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+// Persistor for PersistGate
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
